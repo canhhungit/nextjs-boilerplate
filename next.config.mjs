79388875a359f3f9310715792c2db4820730902a
HungNC <canhhungit@gmail.com>
2024-10-21 14:24:23 +0700

    Update locales

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,65 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from 'next/constants.js' 
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+
+const environment = process.env.NODE_ENV
+
+console.log('Loading nextConfig at:', environment, new Date().toISOString())
+
+const nextConfig = { 
+  // basePath: '/play',
+  output: 'standalone',
+  swcMinify: true,
+  // assetPrefix: '/play',
+  // publicRuntimeConfig: {
+  //   SERVICE_PATH: '/play',
+  // },
+
+  reactStrictMode: false,
+  compiler: {
+    // removeConsole:
+    //   process.env.NODE_ENV === 'production' ? {exclude: ['error']} : false,
+    removeConsole: false,
+  },
+  images: {
+    path: '/play/_next/image',
+    loader: 'default',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+      },
+    ],
+  },
+}
+const nextConfigFunction = async (phase) => {
+  // if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+  //   const withPWA = (await import('@ducanh2912/next-pwa')).default({
+  //     dest: 'public',
+  //     register: true,
+  //     skipWaiting: true,
+  //     // disable: process.env.NODE_ENV === 'development',
+  //     workboxOptions: {
+  //       disableDevLogs: true,
+  //     },
+  //   })
+  //   return withPWA(nextConfig)
+  // }
+  return nextConfig
+}
+
+export default nextConfigFunction
+
+// const withPWA = withPWAInit({
+//   dest: 'public',
+//   disable: process.env.NODE_ENV === 'development',
+//   workboxOptions: {
+//     disableDevLogs: false,
+//   },
+// })
+
+// export default withPWA(nextConfig)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
-import Image from "next/image";
+'use client';
+import PersonalInfo from '@/components/profile/PersonalInfo';
+import PersonalInfoLoading from '@/components/profile/PersonalInfoLoading';
+import Image from 'next/image';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
+import React from 'react';
 
 export default function Home() {
+  const { t } = useTranslation();
+  console.log('Render translation', t('welcome'));
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -11,17 +20,12 @@ export default function Home() {
           width={180}
           height={38}
           priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        />{t('welcome')}
+        <div>
+          <Suspense fallback={<PersonalInfoLoading />}>
+            <PersonalInfo />
+          </Suspense>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -37,7 +41,8 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            {/* Deploy1 */}
+            {t('deploy')}
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
