f53d10a3686b6619dc1f9a2ec4256a9480bb036f
HungNC <canhhungit@gmail.com>
2024-10-23 15:16:06 +0700

    Update generate

--- .plop/settings/component/index.js
+++ .plop/settings/component/index.js
@@ -1,20 +1,20 @@
 module.exports = {
   description: 'Creates a new component',
   prompts: [
-    {
-      type: 'list',
-      name: 'componentType',
-      message: 'What component do you want to create?',
-      choices: ['page', 'component'],
-    },
-    {
-      type: 'input',
-      name: 'other',
-      message: 'còn gì khác không',
-      when: function (answers) {
-        return answers.componentType === 'page';
-      },
-    },
+    // {
+    //   type: 'list',
+    //   name: 'componentType',
+    //   message: 'What component do you want to create?',
+    //   choices: ['page', 'component'],
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'other',
+    //   message: 'còn gì khác không',
+    //   when: function (answers) {
+    //     return answers.componentType === 'page';
+    //   },
+    // },
     {
       type: 'input',
       name: 'name',
@@ -22,16 +22,16 @@ module.exports = {
     },
   ],
   actions: function (data) {
-    let actions = [];
+    let actions = []
 
-    // Tùy theo giá trị của componentType, Plop sẽ tạo file với phần mở rộng tương ứng
-    if (data.componentType === 'page') {
-      actions.push({
-        type: 'add',
-        path: '../src/app/{{camelCase name}}/page.tsx',
-        templateFile: 'templates/component/index.ts.hbs',
-      });
-    } else if (data.componentType === 'component') {
+      // Tùy theo giá trị của componentType, Plop sẽ tạo file với phần mở rộng tương ứng
+      // if (data.componentType === 'page') {
+      //   actions.push({
+      //     type: 'add',
+      //     path: '../src/app/{{camelCase name}}/page.tsx',
+      //     templateFile: 'templates/component/index.ts.hbs',
+      //   });
+      // } else if (data.componentType === 'component') {
       actions.push(
         {
           type: 'add',
@@ -44,8 +44,14 @@ module.exports = {
           path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
           templateFile: 'templates/component/component.tsx.hbs',
         },
+        //Css
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/index.module.scss',
+          templateFile: 'templates/component/scss.hbs',
+        },
       );
-    }
+    // }
 
     return actions;
   },
