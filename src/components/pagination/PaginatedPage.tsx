fa5b6ddbe28d8016bbd16816b4fa66bd7d06c46a
HungNC <canhhungit@gmail.com>
2024-10-28 15:31:41 +0700

    Add Pagination

---
+++ src/components/pagination/PaginatedPage.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+// import { Text } from '@vercel/examples-ui';
+import Pagination from './Pagination';
+
+type PageProps = {
+  products: any[];
+  currentPage: number;
+  totalProducts: number;
+  perPage: number;
+};
+
+const ProductCard = ({ name, description, price }: any) => (
+  <div className="my-10 border-2 border-sky-500 p-3">
+    <span>{name}</span>
+    <span className="my-3">${price}</span>
+    <span className="my-8">{description}</span>
+  </div>
+);
+
+const PaginationPage = ({
+  currentPage,
+  totalProducts,
+  perPage,
+  products,
+}: PageProps): JSX.Element => {
+  return (
+    <div>
+      <span>Page {currentPage}</span>
+      <Pagination
+        totalItems={totalProducts}
+        currentPage={currentPage}
+        itemsPerPage={perPage}
+        renderPageLink={(page) => `/category/${page}`}
+      />
+      <div className="grid grid-cols-3 gap-8">
+        {products.map((product, i) => (
+          <ProductCard key={i} {...product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PaginationPage;
